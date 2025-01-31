4a0be75bb182374fde15a5d3fb5be61916ce5c6e
Akmal Elahi Shaikh <akmalelahishaikh@gmail.com>
2025-01-31 18:58:54 +0500

    feat: rename dashboard to productions

--- src/pages/protected/Welcome.js
+++ src/pages/protected/Welcome.js
@@ -16,7 +16,7 @@ function InternalPage() {
       <div className='hero-content'>
         <div className='max-w-md'>
           <TemplatePointers />
-          <Link to='/app/dashboard'>
+          <Link to='/app/applications'>
             <button className='btn bg-base-100 btn-outline'>Get Started</button>
           </Link>
         </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@
 
 import { lazy } from 'react'
 
-const Dashboard = lazy(() => import('../pages/protected/Dashboard'))
+const Productions = lazy(() => import('../pages/protected/Productions'))
 const Applications = lazy(() => import('../pages/protected/Applications'))
 const Welcome = lazy(() => import('../pages/protected/Welcome'))
 const Page404 = lazy(() => import('../pages/protected/404'))
@@ -21,16 +21,12 @@ const DocComponents = lazy(() => import('../pages/DocComponents'))
 
 const routes = [
   {
-    path: '/dashboard', // the url
-    component: Dashboard // view rendered
-  },
-  {
     path: '/applications',
     component: Applications
   },
   {
     path: '/productions/production1',
-    component: Dashboard
+    component: Productions
   },
   {
     path: '/analytics/*',
