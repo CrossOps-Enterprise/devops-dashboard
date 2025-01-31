9c459320a7be6c43f08938c3a1c7de062ec25331
umairx97 <umairahmedbajwa97@gmail.com>
2025-01-31 15:12:24 +0500

    feat: add primary and secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import { colors } from './src/constants'
+
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
@@ -8,24 +10,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        hd1: '#05f', // dark blue
-        help: '#08f', // light blue
-        hl1: '#f50', // Orange
-        hl1a: '#f504', // Orange with alpha
-        hl2: '#40d', // Purple
-        hl2a: '#40d4', // Purple with alpha
-        hl3: '#888', // Gray
-        hl3a: '#8884', // Gray with alpha
-        bg1: '#fff', // White
-        bg2: '#ddd', // Light gray
-        bg3: '#ccc', // Medium gray
-        brd: '#111', // Dark gray
-        tx1: '#000', // Black
-        tx2: '#fff', // White
-        tx3: '#444', // Dark gray
-        red: '#f00', // Red
-        gre: '#0f0', // Green
-        bro: '#f60' // Orange
+        primary: colors.hd1,
+        secondary: colors.bro
       }
     }
   },
